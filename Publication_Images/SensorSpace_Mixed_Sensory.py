fda910abad2c4fcbfec881fa9d159ecd65c325ca
Emma Bailey <bailey@cbs.mpg.de>
2026-05-18 19:12:59 +0200

    Group level envelope comparison

---
+++ Publication_Images/SensorSpace_Mixed_Sensory.py
@@ -0,0 +1,14 @@
+"""
+Group level amplitude envelope for sensor space data
+Aim is to compare mixed nerve stimulation (wrist) to sensory nerve stimulation (digit12)
+"""
+
+
+import mne
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    
